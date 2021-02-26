6da3effd323629f65fe849cf944bba0b4113ff76
guitongli <guitong.lee.contact@gmail.com>
2021-02-26 14:09:01 +0100

    test ignore

--- public/js/script.js
+++ public/js/script.js
@@ -14,11 +14,34 @@ new Vue({
             .get("/gallery")
             .then(function (imagelist) {
                 console.log(imagelist);
-
+                console.log("down", this);
                 self.images = imagelist.data;
             })
             .catch(function (err) {
                 console.log("error in axios", err);
             });
+    },
+    methods: {
+        handleClick: function (e) {
+            // var file = myFileInput.files[0];
+            var formData = new FormData();
+            formData.append("title", this.title);
+            // formData.append("description", this.description);
+            formData.append("file", this.file);
+            console.log("formdata", formData);
+            console.log("file", this.file);
+            axios
+                .post("/upload", formData)
+                .then((response) => {
+                    console.log(response);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        },
+        handleChange: function (e) {
+            console.log(e.target.files);
+            this.file = e.target.files[0];
+        },
     },
 });
--- server.js
+++ server.js
@@ -3,6 +3,28 @@ const app = express();
 const db = require("./db");
 app.use(express.static("public"));
 
+const multer = require("multer");
+const uidSafe = require("uid-safe");
+const path = require("path");
+
+const diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + "/uploads");
+    },
+    filename: function (req, file, callback) {
+        uidSafe(24).then(function (uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    },
+});
+
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152,
+    },
+});
+
 app.get("/gallery", (req, res) => {
     db.getImgs()
         .then((result) => {
@@ -15,5 +37,13 @@ app.get("/gallery", (req, res) => {
         })
         .catch((err) => console.log(err));
 });
-
+app.post("/upload", uploader.single("file"), (req, res) => {
+    console.log("file", req.file, "body", req.body);
+    if (req.file) {
+        res.json({ succes: true });
+    } else {
+        res.json({ success: false });
+        console.log("why");
+    }
+});
 app.listen(8080, () => console.log("hi"));
