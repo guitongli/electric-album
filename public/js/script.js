1f6406374092aaa85988aeb4c4344c45cfe0cdd1
guitongli <guitong.lee.contact@gmail.com>
2021-03-01 12:32:29 +0100

    debugging

--- public/js/script.js
+++ public/js/script.js
@@ -1,47 +1,64 @@
-console.log("hooked");
-new Vue({
-    el: "#main",
-    data: {
-        name: "Pixel Zoo",
-        images: [],
-    },
+(function () {
+    console.log("hooked");
+    Vue.component("my-component", {
+        template: "template",
+        data: function () {
+            return {
+                name: "guitong",
+                count: 1,
+            };
+        },
+        methods: {
+            updateCount: function () {
+                console.log("zaz");
+                this.count++;
+            },
+        },
+    });
+    new Vue({
+        el: "#main",
+        data: {
+            name: "Pixel Zoo",
+            images: [],
+        },
 
-    mounted: function () {
-        console.log("im mounted");
-        var self = this;
-        console.log("upper", this);
-        axios
-            .get("/gallery")
-            .then(function (imagelist) {
-                console.log(imagelist);
-                console.log("down", this);
-                self.images = imagelist.data;
-            })
-            .catch(function (err) {
-                console.log("error in axios", err);
-            });
-    },
-    methods: {
-        handleClick: function (e) {
-            // var file = myFileInput.files[0];
-            var formData = new FormData();
-            formData.append("title", this.title);
-            // formData.append("description", this.description);
-            formData.append("file", this.file);
-            formData.append("description", this.description);
-            formData.append("username", this.username);
+        mounted: function () {
+            console.log("im mounted");
+            var self = this;
+            console.log("upper", this);
             axios
-                .post("/upload", formData)
-                .then((response) => {
-                    console.log(response);
+                .get("/gallery")
+                .then(function (imagelist) {
+                    console.log(imagelist);
+                    console.log("down", this);
+                    self.images = imagelist.data;
                 })
-                .catch((err) => {
-                    console.log(err);
+                .catch(function (err) {
+                    console.log("error in axios", err);
                 });
         },
-        handleChange: function (e) {
-            console.log(e.target.files);
-            this.file = e.target.files[0];
+        methods: {
+            handleClick: function (e) {
+                // var file = myFileInput.files[0];
+                var formData = new FormData();
+                formData.append("title", this.title);
+                // formData.append("description", this.description);
+                formData.append("file", this.file);
+                formData.append("description", this.description);
+                formData.append("username", this.username);
+                axios
+                    .post("/upload", formData)
+                    .then((response) => {
+                        console.log(response);
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    });
+            },
+            handleChange: function (e) {
+                console.log(e.target.files);
+                this.file = e.target.files[0];
+            },
         },
-    },
-});
+    });
+})();
